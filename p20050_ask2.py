2a573429d65f994789565cc52769d1da5329ee26
chryssagum <76133641+chryssagum@users.noreply.github.com>
2021-01-27 14:04:08 +0200

    Add files via upload

--- p20050_ask2.py
+++ p20050_ask2.py
@@ -1,35 +1,29 @@
 import random
+import sys
 
-print("Please give the n for the nth term")
-term=input()
+print("Please enter the nth term of fib")
+s=int(input())
 count=2
-f1=0
-f2=1
 fn=0
-#print(f1)
-#print(f2)
-while(count<int(term)):
-    fn=f1+f2
-    f1=f2
-    f2=fn
-    #print(fn)
+fn1=1
+fn2=0
+while(count<=s):
+    fn=fn1+fn2
+    fn2=fn1
+    fn1=fn
     count=count+1
-print("The "+term+"th is "+str(fn))
-
-isTrue=True
-count=0
+print(fn)
+random.seed()
+p=fn
+isfound=True
 for i in range(0,20):
-    current_int=random.randint(0,100)
-    if(current_int**fn!=current_int//fn):
-        isTrue=False
-        print("-----")
-        print(fn**current_int)
-        print("\n")
-        print("OK"+str(fn%current_int))
-        count=count+1
-        print(count)
-        print("-----")
-if(isTrue==True):
-    print("It is prime")
-else:
-    print("H xrysa den xerei python")
+    a=random.randint(0,sys.maxsize)
+   # print(a)
+   # print((a**p)%p)
+   # print(a%p)
+    if(((a**p)%p)!=a%p):
+        print("it is not prime")
+        isfound=False
+        break
+if(isfound==True):
+    print("it is prime")
