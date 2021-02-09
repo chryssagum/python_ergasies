01c7c30d65b3fcc29290ec3c2d7e8b301b05b8f5
chryssagum <76133641+chryssagum@users.noreply.github.com>
2021-02-09 16:19:02 +0200

    Add files via upload

---
+++  ask13.py
@@ -0,0 +1,20 @@
+s=open("two.txt","r")
+text=s.read()
+words=text.split()
+counter=0
+length=[]
+pair=[]
+for i in words:
+    length.append(len(i))
+    pair.append(False)
+for i in range(0,len(words)):
+    if(pair[i]!=True):
+        size=len(words[i])
+        pairSize=20-size
+        if(pairSize in length):
+            for j in range(0,len(words)):
+                if(length[j]==pairSize and pair[j]==False and i!=j):
+                    print(words[i]+"\t"+words[j])
+                    pair[i]=True
+                    pair[j]=True
+                    break
